2748176192af9486e7b52d35341764668b21e173
Sarvin Nami <srvn0nm@gmail.com>
2022-02-17 00:36:49 +0330

    Ceramizing the floor area

---
+++ CeramizingTheFloorArea.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int t=0,k=0,n,m,a;
+	scanf("%d %d %d",&n,&m,&a);
+	for(t;(t*a)<n;t++){
+		for(k;(k*a)<m;k++){	
+		}
+	}
+	printf("%d", k*t);
+	return 0;
+}
