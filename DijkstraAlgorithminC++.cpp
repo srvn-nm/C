5eaa90d24f29139a02236235923d952969c503db
Sarvin Nami <srvn0nm@gmail.com>
2023-01-24 19:26:21 +0330

    debugging

--- DijkstraAlgorithminC++.cpp
+++ DijkstraAlgorithminC++.cpp
@@ -26,7 +26,7 @@ Graph::Graph(int V)
 void Graph::addEdge(int u, int v, int w)
 {
     adj[u].push_back(make_pair(v, w));
-    adj[v].push_back(make_pair(u, w));
+    
 }
  
 void Graph::shortestPath(int src)
@@ -66,25 +66,20 @@ void Graph::shortestPath(int src)
 int main()
 {
     
-    int V = 9;
+    int V = 5;
     Graph g(V);
  
-    g.addEdge(0, 1, 4);
-    g.addEdge(0, 7, 8);
-    g.addEdge(1, 2, 8);
-    g.addEdge(1, 7, 11);
-    g.addEdge(2, 3, 7);
-    g.addEdge(2, 8, 2);
-    g.addEdge(2, 5, 4);
-    g.addEdge(3, 4, 9);
-    g.addEdge(3, 5, 14);
-    g.addEdge(4, 5, 10);
-    g.addEdge(5, 6, 2);
-    g.addEdge(6, 7, 1);
-    g.addEdge(6, 8, 6);
-    g.addEdge(7, 8, 7);
+    g.addEdge(1, 2, 0);
+    g.addEdge(3, 2, 0);
+    g.addEdge(1, 3, 9);
+    g.addEdge(2, 5, 6);
+    g.addEdge(2, 4, 0);
+    g.addEdge(4, 3, 0);
+    g.addEdge(5, 1, 0);
+    g.addEdge(5, 4, 6);
+    g.addEdge(4, 1, 6);
  
-    g.shortestPath(0);
+    g.shortestPath(1);
  
     return 0;
 }
